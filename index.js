354e4aab58086fb3bf6c2997a5a7ef8f2ce999dd
Amber Hunt <amberhunt955@gmail.com>
2023-02-14 11:22:16 -0500

    the farm exercise

--- index.js
+++ index.js
@@ -1,26 +1,105 @@
-// Example of What your JS File in VS Code should look like
+//& I. Variables & Data Types
 
+//* A. Q + A
+/* 
+1. How do we assign a value to a variable?
+    Answer -->
+      We assign a value to a variable using =, with the variable name 
+      to the left of the =, and the value assigned to the right of the =
 
-/*
-	Remember!!! Once you’ve completed a problem,
-	COMMENT OUT YOUR WORK before moving on to the next one.
-	This makes it easier to read and debug the current 
-	Solution you are working on.
+2. How do we change the value of a variable?
+    Answer -->
+      We change the value of a variable by setting it = to a new value.
+
+3. How do we assign an existing variable to a new variable?
+    Answer -->
+      const newVariable;
+      existingVariable = newVariable;
+
+4. Remind me, what are declare, assign, and define?
+    Answer -->
+      Declare: create a variable (you can use var, const, let, or nothing)
+      Assign: use = to give a variable a value
+      Define: give the variable a value type
+
+5. What is pseudo coding and why should you do it?
+    Answer -->
+      Pseudo coding is writing the idea of your code in English before
+      attempting to write it in code. This is useful to organize your 
+      thoughts and have a framework to write your code off of.
+
+6. What percentage of time should be spent thinking about how you're 
+going to solve a problem vs actually typing in code to solve it?
+    Answer -->
+      To me, this depends on the person. But personally I like to get a rough
+      idea of what I want to do in pseudo code. Then attempt in code. If you
+      spend enough time thinking about your pseudo code, it shouldn't take you
+      long to write your code at all.
 */
 
+//* B. Strings
 
-// I. Variables & Data Types
-// A. Q + A
-// B. Strings
-// C. Booleans
-// D. The Farm
-// E. Driver's Ed
-//_____________________________________________________________
+// // Create a variable called firstVariable
+// // Assign it the value of the string "Hello World"
+// let firstVariable = "Hello World";
+// // Change the value of this variable to some number
+// firstVariable = 4;
+// // Store the value of firstVariable in a new variable called secondVariable
+// let secondVariable = firstVariable;
+// // Change the value of secondVariable to any string.
+// secondVariable = "string";
+// // What is the value of firstVariable?
+//     // Answer --> 4
+//     console.log(firstVariable);
 
+// // Create a variable called yourName and set it equal to your name as a string. 
+// const yourName = "Amber";
+// // Then, write an expression that takes the string "Hello, my name is " and the 
+// // variable yourName so that it returns a new string with them concatenated.
+// console.log("Hello, my name is " + yourName + ".");
 
+//* C. Booleans
 
+// Using the provided variable definitions, replace the blanks so that 
+// all log statements print true in the console. Answers should be all be 
+// valid JS syntax and not weird things that don't make sense but happen 
+// to print true to the console
 
+// const a = 4;
+// const b = 53;
+// const c = 57;
+// const d = 16;
+// const e = 'Kevin';
 
+// console.log(a < b);
+// console.log(c > d);
+// console.log('Name' === 'Name');
+// // FOR THE NEXT TWO, USE ONLY && OR ||
+// console.log(true || false);
+// console.log(false && false && false && false && false || true);
+// console.log(false === false)
+// console.log(e === 'Kevin');
+// console.log(a + b === c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+// console.log(a * a === d); // note: the answer is a simple arithmetic equation, not something "weird"
+// console.log(48 !== '48');
+
+//* D. The Farm
+
+// Declare a variable animal. Set it to be either "cow" or something else
+let animal = "cow";
+// Write code that will print out "mooooo" if the it is equal to cow
+// Change your code so that if the variable animals anything other than a cow, 
+// it will print "Hey! You're not a cow."
+if (animal === "cow") {
+    console.log("mooooo");
+} else {
+    console.log("Hey! You're not a cow.");
+}
+// Commit
+
+//* E. Driver's Ed
+
+//&_____________________________________________________________
 
 // II. Loops
 // A. The Basics
@@ -28,13 +107,6 @@
 // C. Give me Five
 // D. Savings account
 //_____________________________________________________________
-
-
-
-
-
-
-
 
 // III. Arrays & Control Flow
 // A. Talk about it:
@@ -47,13 +119,6 @@
 // H. What's in Your Closet?
 //_____________________________________________________________
 
-
-
-
-
-
-
-
 // IV. Functions
 // A.
 // B.
@@ -65,11 +130,6 @@
 // H.
 //_____________________________________________________________
 
-
-
-
-
-
 // V. Objects
 // A.
 // B.
@@ -79,6 +139,5 @@
 // F.
 // G.
 //_____________________________________________________________
-
 
 // Extra

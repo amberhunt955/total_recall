72d2f89555411a025567dcafe5211d3a124fdc75
Amber Hunt <amberhunt955@gmail.com>
2023-02-15 10:15:48 -0500

    Get Even answered

--- d2_total_recall.js
+++ d2_total_recall.js
@@ -1,14 +1,22 @@
 //& Easy Going
-// Write a for loop that will log the numbers 1 through 20.
-// The commit message should read: "Easy Going answered"
+// // Write a for loop that will log the numbers 1 through 20.
+// // The commit message should read: "Easy Going answered"
 
-for (let i = 1; i <= 20; i++) {
-    console.log(i);
-}
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i);
+// }
 
 //& ------------------------------
 
 //& Get Even
+// Write a for loop that will log only the even numbers in 0 through 200.
+// Hint: Think about the increment expression.
+// The commit message should read: "Get Even answered"
+
+for (let i = 0; i <= 200; i += 2) {
+    console.log(i);
+}
+
 //& ------------------------------
 
 //& Fizz Buzz

eb17ba56df76e78d63f18c5b6bdc86bf6f6bc19a
Amber Hunt <amberhunt955@gmail.com>
2023-02-15 11:22:53 -0500

    Yell at the Ninja Turtles answered

--- d2_total_recall.js
+++ d2_total_recall.js
@@ -45,37 +45,59 @@
 //& ------------------------------
 
 //& Wild Wild Life
-// Use the following arrays to answer the questions below (name, species, age, hometown): You 
-// should be modifying the elements by accessing them. It is up to you which methods to use.
+// // Use the following arrays to answer the questions below (name, species, age, hometown): You 
+// // should be modifying the elements by accessing them. It is up to you which methods to use.
 
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
 
-// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
-plantee[2]++
-console.log(plantee);
+// // Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+// plantee[2]++
+// console.log(plantee);
 
-// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
-wolfy[3] = "Gotham City";
-console.log(wolfy);
+// // Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// wolfy[3] = "Gotham City";
+// console.log(wolfy);
 
-// Give D'Art a second hometown by adding "Hawkins"
-dart.push("Hawkins");
-console.log(dart);
+// // Give D'Art a second hometown by adding "Hawkins"
+// dart.push("Hawkins");
+// console.log(dart);
 
-// Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy 
-// array and replace it with "Gameboy".
-wolfy[0] = "Gameboy";
-console.log(wolfy);
+// // Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy 
+// // array and replace it with "Gameboy".
+// wolfy[0] = "Gameboy";
+// console.log(wolfy);
 
-// The commit message should read: "Wild Wild Life answered"
+// // The commit message should read: "Wild Wild Life answered"
 
 //& ------------------------------
 
 //& Yell at the Ninja Turtles
+// Create an array with the members of the ninja turtles 
+// (Donatello, Leonardo, Raphael, Michaelangelo)
+
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+
+// Use a for of loop(not a typo - try it out! Try a for of loop) to 
+// call toUpperCase() on each of them and print out the result.
+
+for (let element of ninjaTurtles) {
+    let index = ninjaTurtles.indexOf(element);
+    element = element.toUpperCase();
+    ninjaTurtles[index] = element;
+}
+
+console.log(ninjaTurtles);
+
+// As a developer, you'll be a lifelong learner and constantly encountering 
+// new things. We'll give you little stretches like this to get you used to 
+//looking at documentation and figuring some things out on your own. You've got this!
+
+// The commit message should read: "Yell at the Ninja Turtles answered"
+
 //& ------------------------------
 
 //& Methods, Revisited

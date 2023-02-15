c928bbc08622261c65874db29960287b68682973
Amber Hunt <amberhunt955@gmail.com>
2023-02-15 10:11:28 -0500

    Day 1 finished, Easy Going answered

---
+++ d2_total_recall.js
@@ -0,0 +1,33 @@
+//& Easy Going
+// Write a for loop that will log the numbers 1 through 20.
+// The commit message should read: "Easy Going answered"
+
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
+
+//& ------------------------------
+
+//& Get Even
+//& ------------------------------
+
+//& Fizz Buzz
+//& ------------------------------
+
+//& Wild Wild Life
+//& ------------------------------
+
+//& Yell at the Ninja Turtles
+//& ------------------------------
+
+//& Methods, Revisited
+//& ------------------------------
+
+//& Where is Waldo
+//& ------------------------------
+
+//& Excited Kitten
+//& ------------------------------
+
+//& Find the Median
+//& ------------------------------
--- index.js
+++ index.js
@@ -708,3 +708,76 @@ going to solve a problem vs actually typing in code to solve it?
 //&_____________________________________________________________
 
 //& Extra
+
+// // //* Cat Combinator
+
+// //? 1. Mama cat
+// // Define an object called cat1 that contains the following properties:
+//   // name
+//   // breed
+//   // age (a number)
+
+// const cat1 = {
+//   name: "pete",
+//   breed: "sphynx",
+//   age: 12
+// }
+
+// // console.log the cat's age
+// // console.log the cat's breed
+// console.log(cat1.age);
+// console.log(cat1.breed);
+
+
+
+// //? 2. Papa cat
+// // Define an object called cat2 that also contains the properties:
+//   // name
+//   // breed
+//   // age (a number)
+
+// const cat2 = {
+//   name: "penny",
+//   breed: "siamese",
+//   age: 7
+// }
+
+// //? 3. Combine Cats!
+// // The cats are multiplying!
+
+// // Write a function combineCats that has two parameters mama, and papa. 
+// // The function will take two arguments -- each a cat object. The result 
+// // should be an object where in the name is a concatenation of the 
+// // parents' names, the age is 1, and the breed is each of the parents' 
+// // breeds with a hyphen in between.
+
+// function combineCats(mama, papa) {
+//   const comboCat = {
+//     name: mama.name + papa.name,
+//     age: 1,
+//     breed: mama.breed + "-" + papa.breed
+//   }
+//   console.log(comboCat);
+//   return comboCat;
+// }
+
+// // Pass cat1 and cat2 as arguments to the combineCats function. The function 
+// // should console.log them. 
+
+// combineCats(cat1, cat2);
+
+
+// //? 4. Cat brain bender
+// // If combineCats returns an object, and if combineCats takes objects as arguments, 
+// // then it stands to reason that catCombinator can use itself as its own argument.
+
+// // Take a second to stew on that . . .
+// // What is the result of:
+// console.log(combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2)));
+// // The above console.log is two levels deep of combineCats.
+
+// // Write a console.log that is three levels deep of combineCats. 
+// // combineCats should have two arguments, each which are combineCats, 
+// // each which have two arguments, each which are combineCats.
+
+// console.log(combineCats(combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2)), combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2))));

a3f21aeee73b17cab0c7d51e50337db93a01ebe7
Amber Hunt <amberhunt955@gmail.com>
2023-02-15 12:11:04 -0500

    Where is Waldo answered

--- d2_total_recall.js
+++ d2_total_recall.js
@@ -101,109 +101,129 @@
 //& ------------------------------
 
 //& Methods, Revisited
-// Here is a list of favMovies:
+// // Here is a list of favMovies:
 
-const favMovies = [
-  "Jaws",
-  "The Fellowship of the Ring",
-  "Howl's Moving Castle",
-  "Django Unchained",
-  "Cloud Atlas",
-  "The Usual Suspects",
-  "Toy Story",
-  "Conan the Barbarian",
-  "Titanic",
-  "Harry Potter",
-  "Fried Green Tomatoes",
-  "Volver",
-  "Oculus",
-  "Seven",
-  "Black Panther",
-  "Harry Potter",
-  "Imitation of Life",
-  "Snatch",
-  "Fast and Furious",
-];
+// const favMovies = [
+//   "Jaws",
+//   "The Fellowship of the Ring",
+//   "Howl's Moving Castle",
+//   "Django Unchained",
+//   "Cloud Atlas",
+//   "The Usual Suspects",
+//   "Toy Story",
+//   "Conan the Barbarian",
+//   "Titanic",
+//   "Harry Potter",
+//   "Fried Green Tomatoes",
+//   "Volver",
+//   "Oculus",
+//   "Seven",
+//   "Black Panther",
+//   "Harry Potter",
+//   "Imitation of Life",
+//   "Snatch",
+//   "Fast and Furious",
+// ];
 
-// Console log the index of Titanic
-console.log(favMovies.indexOf("Titanic"));
+// // Console log the index of Titanic
+// console.log(favMovies.indexOf("Titanic"));
 
-// Do the following and console.log the final results -->
+// // Do the following and console.log the final results -->
 
-//? 1. use the .sort method Thought question: what did this 
-//? do to the array? Did it permanently alter it?
-favMovies.sort();
-console.log(favMovies);
-// This rearranged the movies in alphabetical order
-// This action permanently altered the array
+// //? 1. use the .sort method Thought question: what did this 
+// //? do to the array? Did it permanently alter it?
+// favMovies.sort();
+// console.log(favMovies);
+// // This rearranged the movies in alphabetical order
+// // This action permanently altered the array
 
-//? 2. Use the method pop
-favMovies.pop();
-console.log(favMovies);
+// //? 2. Use the method pop
+// favMovies.pop();
+// console.log(favMovies);
 
-//? 3. push "Guardians of the Galaxy"
-favMovies.push("Guardians of the Galaxy");
-console.log(favMovies);
+// //? 3. push "Guardians of the Galaxy"
+// favMovies.push("Guardians of the Galaxy");
+// console.log(favMovies);
 
-//? 4. Reverse the array
-favMovies.reverse();
-console.log(favMovies);
+// //? 4. Reverse the array
+// favMovies.reverse();
+// console.log(favMovies);
 
-//? 5. Use the shift method
-favMovies.shift();
-console.log(favMovies);
+// //? 5. Use the shift method
+// favMovies.shift();
+// console.log(favMovies);
 
-//? 6. unshift - what does it return?
-console.log(favMovies.unshift("JoJo Rabbit"));
-// by using console log we see that unshift returns the length 
-// of the changed array now that we have added a new movie
-console.log(favMovies);
+// //? 6. unshift - what does it return?
+// console.log(favMovies.unshift("JoJo Rabbit"));
+// // by using console log we see that unshift returns the length 
+// // of the changed array now that we have added a new movie
+// console.log(favMovies);
 
-//? 7. splice "Django Unchained" and add "Avatar" (try finding 
-//? the index of "Django Unchained", instead of counting it yourself) 
-//? Thought question: did this permanently alter our array?
-let index = favMovies.indexOf("Django Unchained");
-favMovies.splice(index, 1, "Avatar")
-// This permanently altered the original array
-console.log(favMovies);
+// //? 7. splice "Django Unchained" and add "Avatar" (try finding 
+// //? the index of "Django Unchained", instead of counting it yourself) 
+// //? Thought question: did this permanently alter our array?
+// let index = favMovies.indexOf("Django Unchained");
+// favMovies.splice(index, 1, "Avatar")
+// // This permanently altered the original array
+// console.log(favMovies);
 
-//? 8. slice the last half of the array (challenge yourself and try to 
-//? programmatically determine the middle of the array rather than counting 
-//? it and hard coding it) - Thought question: did this permanently alter our array?
-middleIndex = Math.ceil(favMovies.length / 2);
+// //? 8. slice the last half of the array (challenge yourself and try to 
+// //? programmatically determine the middle of the array rather than counting 
+// //? it and hard coding it) - Thought question: did this permanently alter our array?
+// middleIndex = Math.ceil(favMovies.length / 2);
 
-halvedFavMovies = favMovies.slice(0, middleIndex)
+// halvedFavMovies = favMovies.slice(0, middleIndex)
 
-// We have created a new array, halvedMovies
-console.log(halvedFavMovies);
-// The original array was not modified
-console.log(favMovies);
+// // We have created a new array, halvedMovies
+// console.log(halvedFavMovies);
+// // The original array was not modified
+// console.log(favMovies);
 
-//? 9. store the value of your slice in a variable, console.log 
-//? it - Thought question: what is going on here?
-// I have already done this - created the variable halvedFavMovies
-// We have created a new, separate, array
+// //? 9. store the value of your slice in a variable, console.log 
+// //? it - Thought question: what is going on here?
+// // I have already done this - created the variable halvedFavMovies
+// // We have created a new, separate, array
 
-//? 10. console.log your final results
-// I have done this, above.
+// //? 10. console.log your final results
+// // I have done this, above.
 
-//? 11. After running the above tasks, console.log the index of "Fast and 
-//? Furious" - We removed it from the array, what value do we get when we look 
-//? for the index of something that is not in the array?
-console.log(halvedFavMovies.indexOf("Fast and Furious"));
-// We get -1 as the result, because that element wasn't found in the array
+// //? 11. After running the above tasks, console.log the index of "Fast and 
+// //? Furious" - We removed it from the array, what value do we get when we look 
+// //? for the index of something that is not in the array?
+// console.log(halvedFavMovies.indexOf("Fast and Furious"));
+// // We get -1 as the result, because that element wasn't found in the array
 
-//? 12. Thought question: that we declared the variable favMovies with const, and 
-//? yet, we were allowed to change the array. Weird? Should we have used let?
-// This is because we did not change the ARRAY - we changed 
-// the elements INSIDE the array. If a constant is an object 
-// or array its properties or items can be updated or removed.
+// //? 12. Thought question: that we declared the variable favMovies with const, and 
+// //? yet, we were allowed to change the array. Weird? Should we have used let?
+// // This is because we did not change the ARRAY - we changed 
+// // the elements INSIDE the array. If a constant is an object 
+// // or array its properties or items can be updated or removed.
 
-// -------- The commit message should read: "Methods Revisited answered" -------------
+// // -------- The commit message should read: "Methods Revisited answered" -------------
 
 //& ------------------------------
 
 //& Where is Waldo
+// With the following multi-dimensional array:
+
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+
+// Remove Eggbert (hint look at the slice/splice method(s))
+whereIsWaldo.splice(1, 1)
+console.log(whereIsWaldo);
+
+// Change "Neff" to "No One"
+whereIsWaldo[1][2] = "No One"
+console.log(whereIsWaldo);
+
+// Access and console.log "Waldo"
+let waldo = whereIsWaldo[2][1][1];
+console.log(waldo);
+
+// The commit message should read: "Where is Waldo answered"
+
 //& ------------------------------
 
 //& Excited Kitten

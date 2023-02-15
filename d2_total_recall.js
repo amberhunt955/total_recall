22ae2fdda279d17f9ed9bb2f46eb8f66c6bccda6
Amber Hunt <amberhunt955@gmail.com>
2023-02-15 10:31:54 -0500

    Wild Wild Life answered

--- d2_total_recall.js
+++ d2_total_recall.js
@@ -20,31 +20,59 @@
 //& ------------------------------
 
 //& Fizz Buzz
-// This is a classic problem that you should get really comfortable solving. 
-// If you've solved it before, try to make it more elegant and short.
-// Write a javascript application that logs all numbers from 1 - 100.
-// If a number is divisible by 3 log "Fizz" instead of the number.
-// If a number is divisible by 5 log "Buzz" instead of the number.
-// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
-// The commit message should read: "Fizz Buzz answered"
+// // This is a classic problem that you should get really comfortable solving. 
+// // If you've solved it before, try to make it more elegant and short.
+// // Write a javascript application that logs all numbers from 1 - 100.
+// // If a number is divisible by 3 log "Fizz" instead of the number.
+// // If a number is divisible by 5 log "Buzz" instead of the number.
+// // If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+// // The commit message should read: "Fizz Buzz answered"
 
-for (let i = 1; i <= 100; i++) {
-    // a number divisible by 3 and 5 
-    // will always be divisible by 15
-    if (i % 15 === 0) {
-        console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
-}
+// for (let i = 1; i <= 100; i++) {
+//     // a number divisible by 3 and 5 
+//     // will always be divisible by 15
+//     if (i % 15 === 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
 
 //& ------------------------------
 
 //& Wild Wild Life
+// Use the following arrays to answer the questions below (name, species, age, hometown): You 
+// should be modifying the elements by accessing them. It is up to you which methods to use.
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+
+// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+plantee[2]++
+console.log(plantee);
+
+// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+
+// Give D'Art a second hometown by adding "Hawkins"
+dart.push("Hawkins");
+console.log(dart);
+
+// Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfy 
+// array and replace it with "Gameboy".
+wolfy[0] = "Gameboy";
+console.log(wolfy);
+
+// The commit message should read: "Wild Wild Life answered"
+
 //& ------------------------------
 
 //& Yell at the Ninja Turtles

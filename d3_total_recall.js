51ef8651e6c1721968b41378cc137d816bb944bd
Amber Hunt <amberhunt955@gmail.com>
2023-02-16 09:43:53 -0500

    created person class

--- d3_total_recall.js
+++ d3_total_recall.js
@@ -22,3 +22,50 @@ class Hamster {
 }
     
 // Your commit message should read something like: "created hamster class"
+
+//? Person
+
+class Person {
+    constructor(name){
+      this.name = name
+      this.age = 0
+      this.height = 0
+      this.weight = 0
+      this.mood = 0
+      this.hamsters = []
+      this.bankAccount = 0
+    }
+    getName(){
+      return this.name
+    }
+    getAge(){
+      return this.age
+    }
+    getWeight(){
+      return this.weight
+    }
+    greet(){
+      console.log(`I am ${this.name} hello`)
+    }
+    eat(){
+      this.weight++
+      this.mood++
+    }
+    exercise(){
+      this.weight--
+    }
+    ageUp(){
+      this.age++
+      this.height++
+      this.weight++
+      this.mood--
+      this.bankAccount+=10
+    }
+    buyHamster(hamster){
+      this.hamsters.push(hamster)
+      this.mood+=10
+      this.bankAccount-=hamster.getPrice()
+    }
+  }
+
+  // Your commit message should read something like: "created person class"
